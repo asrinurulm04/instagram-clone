3e3bba3ae9c972fdd1bd795359274d61cbb89029
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-11-28 22:43:28 +0700

    add picture

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   
+  before_create :set_active
+  scope :active, -> { where active:true }
+
+  def set_active
+    self.active = true
+  end
 end
