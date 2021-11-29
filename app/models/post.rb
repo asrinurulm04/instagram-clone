51a5fa733a6e9f543dfd7153e13240133cb3c317
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-11-29 20:40:09 +0700

    add posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
-  
+  belongs_to :account
+
   before_create :set_active
   scope :active, -> { where active:true }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_154014) do
+ActiveRecord::Schema.define(version: 2021_11_29_132648) do
 
   create_table "accounts", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,8 +18,16 @@ ActiveRecord::Schema.define(version: 2021_11_27_154014) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
