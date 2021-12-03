4afe43942fd2785a22e8fb766336e47bacbf565d
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-12-03 22:37:46 +0700

    like post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,16 @@ class LikesController < ApplicationController
 
   def save_like
     @like = Like.new(post_id: params[:post_id], account_id: current_account.id)
-    respond_to do |format|
-      format.json{
+    @post_id = params[:post_id]
+
         if @like.save
-          { success: true }
+          respond_to :js
+          @success = false
         else
-          { success: false }
+          @success = true
         end
-      }
-    end
+
+        render "posts/like"
+  end
 
 end
